7910d28b34f707d427e70c1b4d40bf006445b9e2
Aamna1449 <parveenaamna786@gmail.com>
2026-09-12 21:47:46 +0530

    Add files via upload

---
+++ Day13/LoginCheck.js
@@ -0,0 +1,10 @@
+let username="Aamna";
+let password="1234";
+
+if(username==="Aamna" && password==="1234"){
+    console.log("Login Successful")
+}
+
+else{
+    console.log("Invalid username or password")
+}
---
+++ Day13/nestedd.js
@@ -0,0 +1,16 @@
+let age=21;
+let hasID=false;
+
+if(age>=18){
+    if(hasID==true){
+        console.log("Entry Allowed")
+    }
+    else{
+        console.log("ID required")
+    }
+
+}
+
+else{
+    console.log("Not Allowed")
+}
