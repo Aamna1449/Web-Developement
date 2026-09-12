9d417329bcedcdf41f5e346de42da00b76d17125
Aamna1449 <parveenaamna786@gmail.com>
2026-09-12 21:44:27 +0530

    Add files via upload

---
+++ Day14/Day13/StudentResult.js
@@ -0,0 +1,12 @@
+let marks = 78;
+let attendance = 75;
+
+if (marks >= 90 && attendance >= 75) {
+  console.log("Excellent");
+} else if (marks >= 75 && attendance >= 75) {
+  console.log("Very Good");
+} else if (marks >= 40 && attendance >= 75) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
