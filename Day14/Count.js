856474efb34e103e2e401031287f26dddb0c9b9c
Aamna1449 <parveenaamna786@gmail.com>
2026-09-12 21:48:16 +0530

    Add files via upload

---
+++ Day14/Count.js
@@ -0,0 +1,11 @@
+let count=0;
+
+for(let i=1;i<=20;i++){
+    if(i%3===0){
+        count++;
+    }
+   
+}
+ console.log(count);
+
+ 
---
+++ Day14/Table.js
@@ -0,0 +1,7 @@
+let n=5;
+let table=1;
+
+for(let i=1;i<=10;i++){
+    table=n*i;
+    console.log(table)
+}
